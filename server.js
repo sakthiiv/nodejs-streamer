43bfa98f1bfea3397280f909b33387ae97f3e61f
Sakthivel Viswanathan <visvel@gmail.com>
2016-10-05 17:15:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,24 +6,27 @@ streamer.listen(7000, function(err) {
 	console.log('Server listening in port 7000');
 });
 
-streamer.get('/', function(req, res){
-	console.log(1);
-});
-
-streamer.get('/test', function(req, res){
-	console.log(req);
-});
-
-streamer.post('/test', function(req, res){
-	console.log('Test 1');
+streamer.get('/', function(req, res) {
+	res.write('/, GET');
 	res.end();
 });
 
-streamer.post('/test', function(req, res){
-	console.log('Test 2');
+streamer.get('/test', function(req, res) {
+	res.write('/test, GET');
+	res.end();
+});
+
+streamer.post('/test', function(req, res) {
+	res.write('/test, POST');
+	res.end();
+});
+
+streamer.post('/test', function(req, res) {
+	res.write('/test 2, POST');
 	res.end();	
 });
 
-streamer.post('/authenticate', function(req, res){
-	console.log(req);
+streamer.post('/authenticate', function(req, res) {
+	res.write('/authenticate, POST');	
+	res.end();
 });
